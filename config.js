870bcc35e1a31eb88a1f91fd25556b8aa18fce1f
Caique Nascimento dos Santos <kaiqnasc@gmail.com>
2017-07-12 12:31:21 -0300

    ajustes da listagem de produtos

--- config.js
+++ config.js
@@ -1,11 +1,22 @@
 var mvc = angular.module('FCAMARA', ['ngRoute', 'ngMaterial', 'ngMessages', 'ngStorage']);
-	mvc.run(function ($rootScope, $location, usuariosService) {
+	mvc.run(function ($rootScope, $location, usuariosService, produtosService, $rootScope) {
 	    $rootScope.$on('$routeChangeStart', function (event, next, current) {
 	      if (next.authorize) {
 	      	usuariosService.access().then(sucesso, erro); 	        	        
 	      }
 	      function sucesso(res){
 	      	
+			produtosService.produtos().then(sucess, err);
+
+			function sucess(res){
+
+				$rootScope.produtos = res.data;	
+
+			}
+
+			function err(res){
+				// alert("erro ao carregar os produtos");
+			}
 
 	      }
 
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,15 +1,5 @@
 mvc.controller("homeController", function ($scope, produtosService) {
 
-produtosService.produtos().then(sucesso, erro);
 
-function sucesso(res){
-
-	$scope.produtos = res.data;	
-
-}
-
-function erro(res){
-	alert("erro ao carregar os produtos");
-}
 
 });
